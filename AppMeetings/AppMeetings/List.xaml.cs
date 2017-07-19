5bc30bfe96b78fe14c2bc6cfc5ba6d6ce26f1473
Slim HAMMAMI <slim.hammami.ext@easytek.tn>
2017-07-19 22:03:37 +0100

    first commit

---
+++ AppMeetings/AppMeetings/List.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppMeetings
+{
+    public partial class List : ContentPage
+    {
+
+        private List<Personne> personnes = new List<Personne>();
+        public List()
+        {
+            
+         InitializeComponent();
+        personnes.Add(
+                new Personne
+                {
+                    Nom = "Hammami",
+                    Prenom = "Slim"
+                }
+                );
+            personnes.Add(new Personne
+            {
+                Nom = "Mhamdi",
+                Prenom = "Semah"
+            });
+            MyList.ItemsSource = personnes;
+        }
+
+        private void SearchName(object sender, TextChangedEventArgs e)
+        {
+            string keyWord = MySearchBar.Text;
+            List<Personne> pers =
+                personnes.Where(personne => personne.Nom.ToUpper().Contains(keyWord.ToUpper())).ToList();
+
+            MyList.ItemsSource = pers;
+        }
+    }
+}
---
+++ AppMeetings/AppMeetings/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppMeetings
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMe(object sender, EventArgs e)
+        {
+            DisplayAlert("TEK-UP Summer Trainning", "Bonjour", "Cancel");
+        }
+    }
+}
